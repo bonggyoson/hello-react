4c5969cb6d057d4a3b15611c7c20d8ccb4dd2238
bonggyoson <bonggyoson@gmail.com>
2022-11-23 21:51:22 +0900

    ReactLifeCycleMethod_Practice

--- src/App.js
+++ src/App.js
@@ -5,9 +5,25 @@ import { Component } from 'react';
 // import EventPractice from './EventPractice'
 // import ValidationSample from './ValidationSample'
 // import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
+// import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+// 랜덤 색상을 생성합니다.
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
   render() {
     return (
       // <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
@@ -21,7 +37,13 @@ class App extends Component {
       //     맨 밑으로
       //   </button>
       // </div>
-      <IterationSample />
+      // <IterationSample />
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
     );
   }
 }
